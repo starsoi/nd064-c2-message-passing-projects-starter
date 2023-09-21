0c114e75d181f3f38f739a475c02f5352abede96
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-21 17:05:05 +0200

    fix import errors

--- services/connection/app/protos/person_pb2_grpc.py
+++ services/connection/app/protos/person_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import person_pb2 as person__pb2
+from . import person_pb2 as person__pb2
 
 
 class PersonServiceStub(object):
