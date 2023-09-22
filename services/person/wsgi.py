41e1fd984dd83d1e665a7610ab13eb16895dd5ee
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-22 08:23:48 +0200

    fix grpc

--- services/person/wsgi.py
+++ services/person/wsgi.py
@@ -1,21 +1,24 @@
 import os
 
-from app import create_app
-from app.person.services import PersonService
 import grpc
 from app.protos import person_pb2
 from app.protos import person_pb2_grpc
 from concurrent import futures
-
-app = create_app(os.getenv("FLASK_ENV") or "test")
+from app.person.services import PersonService
 
 grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
 person_pb2_grpc.add_PersonServiceServicer_to_server(PersonService(), grpc_server)
 grpc_server.add_insecure_port("localhost:5005")
+print('gRPC server starting...')
+grpc_server.start()
+print('gRPC server started')
+
+from app import create_app
+print('create app')
+app = create_app(os.getenv("FLASK_ENV") or "test")
+
 
 if __name__ == "__main__":
-    print('gRPC server starting...')
-    grpc_server.start()
-    print('gRPC server started')
+    print('start app')
     app.run(debug=True)
-    print('app exit.')
+    print('app exit')
