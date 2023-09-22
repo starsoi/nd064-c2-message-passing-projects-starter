bf647f56f20eab65fbe77d3c0c9edc00f28220a2
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-22 10:34:40 +0200

    fix grpc server listening address

--- services/person/wsgi.py
+++ services/person/wsgi.py
@@ -8,10 +8,8 @@ from app.person.services import PersonService
 
 grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
 person_pb2_grpc.add_PersonServiceServicer_to_server(PersonService(), grpc_server)
-grpc_server.add_insecure_port("localhost:5005")
-print('gRPC server starting...')
+grpc_server.add_insecure_port("[::]:5005")
 grpc_server.start()
-print('gRPC server started')
 
 from app import create_app
 print('create app')
@@ -19,6 +17,4 @@ app = create_app(os.getenv("FLASK_ENV") or "test")
 
 
 if __name__ == "__main__":
-    print('start app')
     app.run(debug=True)
-    print('app exit')
