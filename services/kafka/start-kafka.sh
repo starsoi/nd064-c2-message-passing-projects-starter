961654499b4978ad21cde915ba5a5157884268d4
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-23 16:22:00 +0200

    add kafka service

---
+++ services/kafka/start-kafka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./bin/zookeeper-server-start.sh config/zookeeper.properties &
+./bin/kafka-server-start.sh config/server.properties &
+
+bin/kafka-topics.sh --create --topic udaconnect-location --bootstrap-server localhost:9092
+
+wait -n
+exit $?
+
