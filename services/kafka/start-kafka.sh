e7e87159e58d463663b5273596a2c8c15d0df265
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-25 15:46:53 +0200

    fix kafka env

--- services/kafka/start-kafka.sh
+++ services/kafka/start-kafka.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://$UDACONNECT_KAFKA_SERVICE_HOST:$UDACONNECT_KAFKA_SERVICE_PORT"
+
 ./bin/zookeeper-server-start.sh config/zookeeper.properties &
 ./bin/kafka-server-start.sh config/server.properties &
 
