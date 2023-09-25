966636a73fef0bff08bde7def35e5afca93742ed
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-25 13:43:21 +0200

    fix kafka dockerfile

--- services/kafka/start-kafka.sh
+++ services/kafka/start-kafka.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 ./bin/zookeeper-server-start.sh config/zookeeper.properties &
 ./bin/kafka-server-start.sh config/server.properties &
 
-bin/kafka-topics.sh --create --topic udaconnect-location --bootstrap-server localhost:9092
+./bin/kafka-topics.sh --create --topic udaconnect-location --bootstrap-server localhost:9092
 
 wait -n
 exit $?
