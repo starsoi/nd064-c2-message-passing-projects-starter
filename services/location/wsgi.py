35c39daa4ac38b0860146851b153cb87358e2b00
Hongyan Zhang <hongyan.zhang@mercedes-benz.com>
2023-09-26 11:54:58 +0200

    fix kafka

--- services/location/wsgi.py
+++ services/location/wsgi.py
@@ -9,14 +9,14 @@ from app.config import SERVICE_URL_KAFKA
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 
-kafka_consumer = KafkaConsumer('udaconnect-location', bootstrap_servers=SERVICE_URL_KAFKA, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+kafka_consumer = KafkaConsumer('udaconnect-location', bootstrap_servers=SERVICE_URL_KAFKA)
 
 def kafka_worker():
     print('kafka worker started.')
     for message in kafka_consumer:
         print(f'new message: {message}')
         with app.app_context():
-            LocationService.create(json.loads(message.decode('utf-8')))
+            LocationService.create(json.loads(message.value.decode('utf-8')))
 
 t = threading.Thread(target=kafka_worker)
 t.start()
